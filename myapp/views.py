5fdf81b44d729e9d54e81b51b7fdf140b4e21b57
pradeephuded37 <pradeephuded37@gmail.com>
2020-08-13 17:34:41 +0530

    fourth

--- myapp/views.py
+++ myapp/views.py
@@ -59,9 +59,13 @@ def multi(request):
 from django.core.files.storage import FileSystemStorage
 
 def img_upload(request):
+    file_url=False
     if request.method=="POST" and request.FILES:
         image=request.FILES['sam']
         fs=FileSystemStorage()
-        fs.save(image.name,image)
+        file=fs.save(image.name,image)
+        file_url=fs.url(file)
                 
-    return render(request,"img_upload.html")
+    return render(request,"img_upload.html",context={'file_url':file_url})
+
+
